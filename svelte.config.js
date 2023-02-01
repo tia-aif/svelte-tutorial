fac3528d768580eb0b8b54814ecf31579a34589c
riamthoj <riamthoj2019@gmail.com>
2023-02-01 13:45:54 +0700

    fix-docker

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
-import adapterStatic from '@sveltejs/adapter-static';
-import adapterNode from '@sveltejs/adapter-node';
+// import adapterStatic from '@sveltejs/adapter-static';
+// import adapterNode from '@sveltejs/adapter-node';
 // import adapterMulti from '@macfja/svelte-multi-adapter';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
